b4aac345f3e7f5def8e3a816da74d39283305f74
myusername <s.jman@yandex.com>
2024-06-01 20:19:39 +0400

    Update admin.py

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from .models import User, Listing, Bid, Comment, Category, Watchlist
+from .models import User, Listing, Comment, Category, Watchlist
 
 admin.site.register(User)
 admin.site.register(Listing)
-admin.site.register(Bid)
 admin.site.register(Comment)
 admin.site.register(Category)
 admin.site.register(Watchlist)
